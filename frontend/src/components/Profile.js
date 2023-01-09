3dc8272ba239c232eb12025cbf1bf013a3db4619
h_k_rony <raindropplus11@gmail.com>
2023-01-10 04:02:06 +0600

    add // eslint-disable-next-line

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -23,6 +23,7 @@ const Profile = () => {
 
   useEffect(() => {
     getData()
+    // eslint-disable-next-line
   }, []);
 
 
--- frontend/src/components/UserDetails.js
+++ frontend/src/components/UserDetails.js
@@ -26,6 +26,7 @@ const UserDtetails = () => {
 
   useEffect(() => {
     getData()
+    // eslint-disable-next-line
   }, []);
 
   return (
